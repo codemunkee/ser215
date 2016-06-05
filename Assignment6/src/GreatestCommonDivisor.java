647e6a0a624fad39a1b2df7d2c688f06af8937e1
Russ Nealis <rnealis@twilio.com>
2016-06-05 07:17:09 -0700

    Assignment 6 - GCD, better validation logic to make sure the user doesn't enter 0 (which is never divisible :-)

--- Assignment6/src/GreatestCommonDivisor.java
+++ Assignment6/src/GreatestCommonDivisor.java
@@ -36,34 +36,79 @@ public class GreatestCommonDivisor {
         // get the number pairs from the user
         for (int queryCount = 0; queryCount < pairCount; queryCount++) {
 
-            System.out.printf("\nEnter Data for Pair %s of %s\n", queryCount + 1, pairCount);
+            System.out.printf("\nEnter Data for Pair %s of %s\n\n", queryCount + 1, pairCount);
 
             // We query the user twice, one for each number of the pair,
             // there is code duplication here but it helps with readability
 
-            System.out.print("\nEnter 1st number of pair: ");
-            while(!scan.hasNextInt()) {
-                System.out.println("\nYou must provide a number.\n");
+            do {
                 System.out.print("Enter 1st number of pair: ");
-                scan.next();
-            }
-            numberPairs[queryCount][0] = scan.nextInt();
+                while (!scan.hasNextInt()) {
+                    System.out.println("\nYou must provide a number.\n");
+                    System.out.print("Enter 1st number of pair: ");
+                    scan.next();
+                }
+                numberPairs[queryCount][0] = scan.nextInt();
+                if (numberPairs[queryCount][0] == 0) {
+                    System.out.println("\nYou will never find a divisor for 0.\n");
+                }
+            } while (numberPairs[queryCount][0] == 0);
 
-            System.out.printf("Enter 2nd number of pair: ");
-            while(!scan.hasNextInt()) {
-                System.out.println("\nYou must provide a number.\n");
-                System.out.printf("Enter 2nd number of pair: ");
-                scan.next();
-            }
-            numberPairs[queryCount][1] = scan.nextInt();
+
+            do {
+                System.out.print("Enter 2nd number of pair: ");
+                while (!scan.hasNextInt()) {
+                    System.out.println("\nYou must provide a number.\n");
+                    System.out.print("Enter 2nd number of pair: ");
+                    scan.next();
+                }
+                numberPairs[queryCount][1] = scan.nextInt();
+                if (numberPairs[queryCount][1] == 0) {
+                    System.out.println("\nYou will never find a divisor for 0.\n");
+                }
+            } while (numberPairs[queryCount][1] == 0);
+
         }
+
+        // Blank line to separate user input from next console output
+        System.out.println();
 
         return numberPairs;
     }
 
+    public static int getGCD(int A, int B) {
+        /* Takes two integers and finds their greatest common divisor by
+           using Euclid's division algorithm.
+         */
+
+        // we want A to be greater than B, if that's not the case,
+        // make it so it is
+        if (B > A) {
+            int tmp = A;
+            A = B;
+            B = tmp;
+        }
+
+        while (true) {
+            int C = A % B;
+            // if there isn't a remainder when dividing the two, we've
+            // found the GCD
+            if (C == 0) {
+                return B;
+            } else {
+                A = B;
+                B = C;
+            }
+        }
+    }
 
     public static void main(String args[]) {
         // Get number pairs from the user
         int[][] numberPairs = getUserInput();
+
+        // Go through the pairs and print each as well as their GCD
+        for (int[] pair : numberPairs) {
+            System.out.printf("%s %s %s\n", pair[0], pair[1], getGCD(pair[0], pair[1]));
+        }
     }
 }
